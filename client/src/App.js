5ebbe18c2cbcab1ed40a8d03fb518c54d89897ce
Alvin Savoy <asavoy@users.noreply.github.com>
2018-06-08 17:22:35 +1000

    Client: add a stripped down create-react-app client

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    testResponse: "Waiting for response..."
+  };
+  async componentDidMount() {
+    const response = await fetch("/api/test");
+    const message = await response.json();
+    this.setState({
+      testResponse: message
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Example React &amp; Express App</h1>
+        </header>
+        <p className="App-intro">
+          Testing response from server: <code>{this.state.testResponse}</code>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
